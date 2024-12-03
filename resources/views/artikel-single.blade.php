4cc38dc2c1703b391d6a51438dc46ef624cedfe3
Aditya <adityaprasetyasuryana@gmail.com>
2024-12-04 00:37:10 +0700

    test

--- resources/views/artikel-single.blade.php
+++ resources/views/artikel-single.blade.php
@@ -8,67 +8,65 @@
 @include('layouts.navbar')
 
 <div class="container">
-        <div class="row">
+    <div class="row">
 
-            <!-- Kolom Konten Artikel -->
-            <div class="col-lg-8 mb-5 mb-lg-0">
-                <div class="post-content post-single">
-                    <!-- Menampilkan gambar artikel -->
-                    <div class="post-media post-image">
-                        <img loading="lazy" src="{{ asset('storage/' . $blog->image) }}" class="img-fluid mt-3" alt="post-image">
-                    </div>
+        <div class="col-lg-8 mb-5 mb-lg-0">
 
-                    <div class="post-body">
-                        <div class="entry-header">
-                            <div class="post-meta">
-                                <!-- Menampilkan penulis artikel -->
-                                <span class="post-author">
-                                    <i class="fas fa-user"></i>{{ $blog->author->name }}
-                                </span>
-                                <!-- Menampilkan tanggal artikel -->
-                                <span class="post-meta-date"><i class="far fa-calendar"></i>{{ $blog->created_at->format('l, d F Y') }}</span>
-                            </div>
-                            <!-- Menampilkan judul artikel -->
-                            <h2 class="entry-title">{{ $blog->title }}</h2>
-                        </div>
-
-                        <div class="entry-content">
-                            <!-- Menampilkan konten artikel -->
-                            <div>{!! nl2br(e($blog->content)) !!}</div>
-                        </div>
-                    </div>
+            <div class="post-content post-single">
+                <div class="post-media post-image">
+                    <img loading="lazy" src="assets/1724903464.png" class="img-fluid mt-3" alt="post-image">
                 </div>
-            </div><!-- Content Col end -->
 
-            <!-- Kolom Sidebar -->
-            <div class="col-lg-4 mt-3">
-                <div class="sidebar sidebar-right">
-                    <div class="widget recent-posts">
-                        <h3 class="widget-title">Recent Posts</h3>
-                        <ul class="list-unstyled">
-                            @foreach($blogs as $recentBlog)
-                                <li class="d-flex align-items-center">
-                                    <div class="posts-thumb">
-                                        <!-- Thumbnail artikel -->
-                                        <a href="{{ route('blog.show', $recentBlog->slug) }}">
-                                            <img loading="lazy" alt="img" src="{{ asset('storage/' . $recentBlog->image) }}" style="height: 50px; width: 80px;">
-                                        </a>
-                                    </div>
-                                    <div class="post-info">
-                                        <h4 class="entry-title">
-                                            <a href="{{ route('blog.show', $recentBlog->slug) }}">{{ $recentBlog->title }}</a>
-                                        </h4>
-                                    </div>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div><!-- Recent Posts Widget -->
-                </div><!-- Sidebar end -->
-            </div><!-- Sidebar Col end -->
+                <div class="post-body">
+                    <div class="entry-header">
+                        <div class="post-meta">
+                            <span class="post-author">
+                                <i class="fas fa-user"></i>Dewiku
+                            </span>
+                            <span class="post-meta-date"><i class="far fa-calendar"></i>Kamis, 29 Agustus 2024</span>
+                        </div>
+                        <h2 class="entry-title">
 
-        </div><!-- Main row end -->
-    </div><!-- Container end -->
+                        </h2>
+                    </div>
 
+                </div>
+            </div>
+
+        </div>
+        <div class="col-lg-4 mt-3">
+
+            <div class="sidebar sidebar-right">
+                <div class="widget recent-posts">
+                    <h3 class="widget-title">Recent Posts</h3>
+                    <ul class="list-unstyled">
+                                                <li class="d-flex align-items-center">
+                            <div class="posts-thumb">
+                                <a href="#"><img loading="lazy" alt="img" src="assets/1725275239.jpg" style="height: 50px; Width: 80px;"></a>
+                            </div>
+                            <div class="post-info">
+                                <h4 class="entry-title">
+                                    <a href="#">Sejarah Koperasi</a>
+                                </h4>
+                            </div>
+                        </li>
+                                                <li class="d-flex align-items-center">
+                            <div class="posts-thumb">
+                                <a href="#"><img loading="lazy" alt="img" src="assets/1724903464.png" style="height: 50px; Width: 80px;"></a>
+                            </div>
+                            <div class="post-info">
+                                <h4 class="entry-title">
+                                    <a href="#">Koperasi Jantungnya Roda Perekonomian Bangsa</a>
+                                </h4>
+                            </div>
+                        </li>
+                        
+                    </ul>
+
+                </div>
+            </div><!-- Main row end -->
+        </div>
+    </div>
 </div>
 
 <!-- Footer -->
