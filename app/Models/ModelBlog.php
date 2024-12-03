f0e700b7adacd33561105136693c1506140d2815
Aditya <adityaprasetyasuryana@gmail.com>
2024-12-03 19:30:06 +0700

    Update Model

---
+++ app/Models/ModelBlog.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModelBlog extends Model
+{
+    use HasFactory;
+
+    // Menentukan nama tabel, karena nama tabel secara default adalah bentuk jamak dari nama model
+    protected $table = 'blog';
+
+    // Menentukan kolom-kolom yang dapat diisi secara massal (mass-assignment)
+    protected $fillable = [
+        'title', 
+        'content', 
+        'author_id', 
+        'status', 
+        'published_at',
+    ];
+
+    // Menentukan relasi dengan model User (author)
+    public function author()
+    {
+        return $this->belongsTo(ModelUser::class, 'author_id');
+    }
+
+    // Menentukan apakah kolom timestamps digunakan secara otomatis
+    public $timestamps = true;
+}
--- app/Models/ModelUser.php
+++ app/Models/ModelUser.php
@@ -11,22 +11,21 @@ class ModelUser extends Authenticatable
     use HasApiTokens, Notifiable;
 
     // Tentukan nama tabel yang digunakan
-    protected $table = 'as_users';
+    protected $table = 'users';
 
     // Tentukan primary key jika berbeda dari 'id'
-    protected $primaryKey = 'user_id';
+    protected $primaryKey = 'id';
 
     // Tentukan apakah primary key auto increment
     public $incrementing = false;
 
     // Tentukan kolom yang dapat diisi secara massal (mass assignment)
     protected $fillable = [
-        'username',
-        'password',
-        'role',
         'name',
         'email',
-        'phone',
+        'email_verified_at',
+        'password',
+        'remember_token',
     ];
 
     protected $hidden = [
