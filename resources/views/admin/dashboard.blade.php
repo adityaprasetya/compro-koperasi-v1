3c6a86f9c1b07aa7568fa97177da3c994581b177
Aditya <adityaprasetyasuryana@gmail.com>
2024-12-04 03:49:23 +0700

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,8 +1,8 @@
 <!-- Header -->
 @include('admin.layouts.header')
 
-<body class="g-sidenav-show   bg-gray-100">
-  <div class="min-height-300 bg-dark position-absolute w-100"></div>
+<body class="g-sidenav-show   ">
+  <div class="min-height-300  position-absolute w-100"></div>
 
   <!-- Sidebar -->
   @include('admin.layouts.sidebar')
