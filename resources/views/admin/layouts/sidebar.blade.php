2c9fb9ad6610f93843f6c18a2fe40a61577e17ea
Aditya <adityaprasetyasuryana@gmail.com>
2024-12-03 20:31:51 +0700

    update sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,8 +2,8 @@
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="{{ url('/dashboard') }}">
-        <img src="../assets/admin/img/logo-ct-dark.png" width="26px" height="26px" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold">AppSekolah</span>
+        <img src="assets/login/logo-gh-test.png" width="26px" height="26px" class="navbar-brand-img h-100" alt="main_logo">
+        <span class="ms-1 font-weight-bold">TMI Syariah - Telaga Mandiri Indonesia</span>
       </a>
     </div>
     <hr class="horizontal dark mt-0">
