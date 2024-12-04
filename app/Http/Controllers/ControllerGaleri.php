aa7fdf865226bd0bfc1e587eba62d77bc631b32b
Aditya <adityaprasetyasuryana@gmail.com>
2024-12-04 23:58:18 +0700

    update controller galeri

--- app/Http/Controllers/ControllerGaleri.php
+++ app/Http/Controllers/ControllerGaleri.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\ModelGaleri;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ControllerGaleri extends Controller
 {
