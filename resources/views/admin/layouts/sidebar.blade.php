5bde1769af986b0cf4e6b06bc0f9470e046f9913
Aditya <adityaprasetyasuryana@gmail.com>
2024-12-05 00:25:35 +0700

    update sidebar icon

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -42,7 +42,7 @@
         <li class="nav-item">
           <a class="nav-link {{ Request::is('blog') ? 'active' : '' }}" href="{{ url('/blog') }}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="ni ni-app text-dark text-sm opacity-10"></i>
+              <i class="ni ni-paper-diploma text-dark text-sm opacity-10"></i>
             </div>
             <span class="nav-link-text ms-1">Blog</span>
           </a>
@@ -71,7 +71,7 @@
                 @csrf
                 <button type="submit" class="nav-link" style="background: none; border: none;">
                     <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-single-copy-04 text-dark text-sm opacity-10"></i>
+                        <i class="ni ni-bold-left text-dark text-sm opacity-10"></i>
                     </div>
                     <span class="nav-link-text ms-1">Keluar</span>
                 </button>
