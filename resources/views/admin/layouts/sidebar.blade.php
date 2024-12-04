68f4d299c2bf33983de4dfcdbfd55a1c57477ed2
Aditya <adityaprasetyasuryana@gmail.com>
2024-12-05 00:58:21 +0700

    update sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -69,7 +69,7 @@
         <li class="nav-item">
           <a class="nav-link {{ Request::is('sliders') ? 'active' : '' }}" href="{{ url('/sliders') }}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="ni ni-camera-compact text-dark text-sm opacity-10"></i>
+              <i class="ni ni-album-2 text-dark text-sm opacity-10"></i>
             </div>
             <span class="nav-link-text ms-1">Sliders</span>
           </a>
