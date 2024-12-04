d898cb6adeee30d68e820a402184e437740fbb3c
Aditya <adityaprasetyasuryana@gmail.com>
2024-12-05 01:02:29 +0700

    update view sliders

--- resources/views/admin/sliders.blade.php
+++ resources/views/admin/sliders.blade.php
@@ -15,8 +15,6 @@
     <div class="col-12">
         <div class="card mb-4">
             <div class="card-header pb-0">
-                <h6>Tambah Slider</h6>
-            </div>
             <div class="card-body px-0 pt-0 pb-2">
                 <form action="{{ route('sliders.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
@@ -53,6 +51,7 @@
                         <button type="submit" class="btn btn-primary">Simpan</button>
                     </div>
                 </form>
+            </div>
             </div>
         </div>
     </div>
