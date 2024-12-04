97b864d0beedcb90fa8ef25ff1d9ac5e8e7fb25c
Aditya <adityaprasetyasuryana@gmail.com>
2024-12-05 03:18:42 +0700

    hilangkan tombolcarousel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -102,14 +102,14 @@
         @endforeach
     </div>
     <!-- Controls -->
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden"></span>
     </button>
     <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden"></span>
-    </button>
+    </button> -->
 </div>
 
 <section class="call-to-action no-padding">
