acaa7327af77b850b40a349d6f6a62462fcf2ab6
Aditya <adityaprasetyasuryana@gmail.com>
2024-12-07 17:57:11 +0700

    update pembiayaan view

--- resources/views/pinjaman/pembiayaantmi.blade.php
+++ resources/views/pinjaman/pembiayaantmi.blade.php
@@ -9,16 +9,18 @@
 
 <section id="main-container" class="main-container">
     <div class="container">
-        <div class="row">
-            <h3 class="column-title text-center">Pembiayaan TMI</h3>
-            <div class="row justify-content-md-center">
-                <!-- Menampilkan gambar yang besar dan responsif -->
-                @foreach ($pembiayaans as $pembiayaan)
-                <div class="col-12 col-md-8">
+        <div class="row justify-content-center">
+            <!-- Judul di atas gambar -->
+            <h3 class="column-title text-center mb-4">Pembiayaan TMI</h3>
+        </div>
+        
+        <div class="row justify-content-center">
+            <!-- Menampilkan gambar yang besar dan responsif -->
+            @foreach ($pembiayaans as $pembiayaan)
+                <div class="col-12 col-md-10">
                     <img src="{{ url('storage/pembiayaan/' . $pembiayaan->image) }}" class="img-fluid" alt="Pembiayaan TMI Image">
                 </div>
-                @endforeach
-            </div>
+            @endforeach
         </div>
     </div><!-- Container end -->
 </section><!-- Main container end -->
